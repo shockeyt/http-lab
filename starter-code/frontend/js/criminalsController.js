c8deb755b269878652dae7583342a95bb6a22452
shockeyt <shockeyt@gmail.com>
2017-04-26 17:13:46 -0600

    delete works

--- starter-code/frontend/js/criminalsController.js
+++ starter-code/frontend/js/criminalsController.js
@@ -7,7 +7,7 @@ function CriminalsController($http) {
 	var self = this;
 	self.all = [];
 
-
+	//GET all
 	function getCriminals() {
 		//console.log("get criminal is working");
 		$http
@@ -19,6 +19,49 @@ function CriminalsController($http) {
 	}
 
 	getCriminals();
+
+
+	this.addCriminal = addCriminal;
+	this.newCriminal = {};
+
+
+	//POST
+	function addCriminal() {
+		this.all.push(this.newCriminal);
+		$http
+		.post('http://localhost:3000/criminals', this.newCriminal)
+		.then(function(response) {
+			console.log(response);
+		});
+		self.newCriminal = {};
+	}
+
+	// function showCriminal() {
+	// 	$http
+	// 	.get('http://localhost:3000/criminals/:id')
+	// 	.then(function(response) {
+	// 		self.all = response.data;
+	// 		console.log(self.all);
+	// 	});
+	// }
+
+	this.deleteCriminal = deleteCriminal;
+
+	function deleteCriminal(criminal) {
+		console.log("delete button clicked");
+
+		  $http
+		  .delete('http://localhost:3000/criminals/' + criminal._id)
+		  .then(function(response){
+		  	console.log(response);
+		  	//var index = self.all.index;
+		  	var index = self.all.indexOf(criminal);
+		  	self.all.splice(index, 1);
+		  	//self.$emit("criminal deleted", criminal);
+		  });
+		
+	}
+
 
 
 
